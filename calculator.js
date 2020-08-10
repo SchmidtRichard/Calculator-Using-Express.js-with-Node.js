8f12f0947286ac6d803bf4d2ba6c5f65ccf12ce3
SchmidtRichard <ri.schmidta@gmail.com>
2020-08-10 13:02:32 +0100

    Responding to Requests with HTML Files - API - res.sendFile(path [, options] [, fn])

--- calculator.js
+++ calculator.js
@@ -5,7 +5,13 @@ const app = express();
 
 //Create a root route get method with app.get()
 app.get("/", function(request, response) {
-  response.send("I am a calculator");
+  //API - res.sendFile(path [, options] [, fn])
+  //Transfers the file to the brownser when they make a GET request
+  response.sendFile(__dirname + "/index.html");
+
+  //console.log(__dirname);
+
+
 });
 
 //Spin up the server on port 3000 with app.listen

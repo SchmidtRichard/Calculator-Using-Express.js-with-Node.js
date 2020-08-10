6d08570b6d5b223a950ade634d5ee8df9116854d
SchmidtRichard <ri.schmidta@gmail.com>
2020-08-10 18:21:28 +0100

    BMI Routing Challenge

--- calculator.js
+++ calculator.js
@@ -6,11 +6,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+/*
+npm install body-parser
+Process and send the result back - Install body parse
+Allow to parse the info we get sent from the POST request so we
+have access to properties and variables so we can do our calculation
+*/
+
 //Get the app to use body-parser
 //urlencoded used when trying to parse data coming from a HTML form
 app.use(bodyParser.urlencoded({
   extended: true //allows to post nested objects - body-parse requires it to be explicitled declared even if not usuing the extended: true
 }));
+
+/* CALCULATOR ROUTES */
 
 //Create a root route get method with app.get()
 app.get("/", function(request, response) {
@@ -18,7 +27,7 @@ app.get("/", function(request, response) {
   //Transfers the file to the brownser when they make a GET request
   response.sendFile(__dirname + "/index.html");
 
-  //console.log(__dirname);
+  console.log(__dirname);
 });
 
 //Process POST requests
@@ -37,16 +46,25 @@ app.post("/", function(request, response) {
   response.send("The result of the calculation is " + result);
 });
 
-/*
-npm install body-parser
-Process and send the result back - Install body
-Allow to parse the info we get sent from the POST request so we
-have access to properties and variables so we can do our calculation
-*/
+/* BMI CALCULATOR ROUTES */
 
+app.get("/bmicalculator", function(request, response) {
+  response.sendFile(__dirname + "/bmiCalculator.html");
 
+  console.log(__dirname);
+});
+
+app.post("/bmicalculator", function(request, response) {
+
+  var weight = parseFloat(request.body.weight);
+  var height = parseFloat(request.body.height);
+
+  var bmi = weight / (height * height);
+
+  response.send("Your BMI is " + bmi);
+});
 
 //Spin up the server on port 3000 with app.listen
 app.listen(3000, function() {
-  console.log("server started on port 3000 🚀");
+  console.log("Server started on port 3000");
 });
